aca26e573847b8a6344566536f538e973baaf038
pratiksharm <sharma.pratik2016@gmail.com>
2021-04-12 15:43:24 +0530

    added a lot of changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import '@/css/tailwind.css'
+/* eslint-disable prettier/prettier */
+import "@/css/tailwind.css";
 
-import { MDXProvider } from '@mdx-js/react'
-import { ThemeProvider } from 'next-themes'
-import { DefaultSeo } from 'next-seo'
-import Head from 'next/head'
+import { MDXProvider } from "@mdx-js/react";
+import { ThemeProvider } from "next-themes";
+import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 
-import { SEO } from '@/components/SEO'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import MDXComponents from '@/components/MDXComponents'
+import { SEO } from "@/components/SEO";
+import LayoutWrapper from "@/components/LayoutWrapper";
+import MDXComponents from "@/components/MDXComponents";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +23,5 @@ export default function App({ Component, pageProps }) {
         </LayoutWrapper>
       </MDXProvider>
     </ThemeProvider>
-  )
+  );
 }
--- pages/map.js
+++ pages/map.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react'
-import India from './map/india'
-import { SVGMap } from 'react-svg-map'
-import 'react-svg-map/lib/index.css'
-import { getLocationId, getLocationName } from './map/utils'
+import React, { useState } from "react";
+import India from "./map/india";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import { getLocationId, getLocationName } from "./map/utils";
 
 export default function MapPage() {
-    const [pointedLocation, setPointed] = useState(null)
-    const [focusedLocation, setFocused] = useState(null)
-    const [clickedLocation, setClicked] = useState(null)
+  const [pointedLocation, setPointed] = useState(null);
+  const [focusedLocation, setFocused] = useState(null);
+  const [clickedLocation, setClicked] = useState(null);
 
-    const links = {
-        an: '/map/AndamanAndNicobarIslands',
-        ap: '/map/AndraPradesh',
-        ar: '/map/ArunachalPradesh',
-        as: '/map/Assam',
-        br: '/map/Bihar',
-        ch: '/map/Chandigarh',
-        ct: '/map/Chattisgarh',
-        dn: '/map/DadraAndNagarHaveli',
-        dd: '/map/DamanAndDiu',
-        dl: '/map/Delhi',
-        ga: '/map/Goa',
-        gj: '/map/Gujarat',
-        hr: '/map/Haryana',
-        hp: '/map/HimachalPradesh',
-        jk: '/map/JammuAndKashmir',
-    }
-    const handleLocationClick = (e) => {
-        const clickedLocation = getLocationName(e)
-        const clickedLocationId = getLocationId(e)
-        setClicked(clickedLocation)
-        window.open(links[clickedLocationId], '_self')
-    }
+  const links = {
+    an: "/map/AndamanAndNicobarIslands",
+    ap: "/map/AndraPradesh",
+    ar: "/map/ArunachalPradesh",
+    as: "/map/Assam",
+    br: "/map/Bihar",
+    ch: "/map/Chandigarh",
+    ct: "/map/Chattisgarh",
+    dn: "/map/DadraAndNagarHaveli",
+    dd: "/map/DamanAndDiu",
+    dl: "/map/Delhi",
+    ga: "/map/Goa",
+    gj: "/map/Gujarat",
+    hr: "/map/Haryana",
+    hp: "/map/HimachalPradesh",
+    jk: "/map/JammuAndKashmir",
+  };
+  const handleLocationClick = (e) => {
+    const clickedLocation = getLocationName(e);
+    const clickedLocationId = getLocationId(e);
+    setClicked(clickedLocation);
+    window.open(links[clickedLocationId], "_self");
+  };
 
-    return (
-        <div>
-            <h1>This is the home page</h1>
-            <SVGMap
-                className="stroke-current stroke-1 text-grey-600 dark:text-white"
-                onLocationClick={(e) => handleLocationClick(e)}
-                map={India}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="text-grey-600">Click on the state to know more...</h2>
+      <SVGMap
+        className="stroke-current stroke-1 text-grey-600 dark:text-white"
+        onLocationClick={(e) => handleLocationClick(e)}
+        map={India}
+      />
+    </div>
+  );
 }
